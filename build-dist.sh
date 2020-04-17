741f5da286b71768e9985b0a476d213d63be18d9
Charlie Qiu <qbowater@gmail.com>
2020-04-17 23:49:32 +0800

    Update build-sit.sh

--- build-dist.sh
+++ build-dist.sh
@@ -6,6 +6,18 @@ mkdir ./dist
 VER=`git describe --tags --always --long`
 echo Version: $VER
 
+sum=""
+if hash shasum 2>/dev/null; then
+    sum="shasum"
+fi
+
+
+[[ -z $upx ]] && upx="echo pending"
+if [[ $upx == "echo pending" ]] && hash upx 2>/dev/null; then
+	upx="upx -9"
+fi
+
+
 platforms=("darwin/amd64" "linux/amd64" "linux/386" "windows/amd64" "windows/386")
 for platform in "${platforms[@]}"
 do
@@ -13,5 +25,21 @@ do
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
 
-    env CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -ldflags "-X 'github.com/qiuyuzhou/h2tun/cmd/h2tun/cmd.version=$VER'" -o ./dist/h2tun-$GOOS-$GOARCH ./cmd/h2tun
+    suffix=""
+    if [ "$GOOS" == "windows" ]
+    then
+        suffix=".exe"
+    fi
+
+    env CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -ldflags \
+        "-X 'github.com/qiuyuzhou/h2tun/cmd/h2tun/cmd.version=$VER'" \
+        -o ./dist/h2tun-$GOOS-${GOARCH}${suffix} ./cmd/h2tun
+    $upx dist/h2tun-${GOOS}-${GOARCH}${suffix} > /dev/null
+    tar -zcf dist/h2tun-${GOOS}-${GOARCH}-$VER.tar.gz dist/h2tun-${GOOS}-${GOARCH}${suffix}
+    if [ "$sum" != ""  ]
+    then
+        cd dist
+        $sum h2tun-${GOOS}-${GOARCH}-$VER.tar.gz
+        cd ..
+    fi
 done
